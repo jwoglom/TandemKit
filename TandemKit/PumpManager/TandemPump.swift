c7010cec5b24de29f297ec8c6e59a9a5005dbe05
James Woglom <j@wogloms.net>
2025-07-20 20:47:27 -0400

    more

--- TandemKit/PumpManager/TandemPump.swift
+++ TandemKit/PumpManager/TandemPump.swift
@@ -11,6 +11,11 @@ import CoreBluetooth
 import LoopKit
 import OSLog
 
+public protocol TandemPumpDelegate: AnyObject {
+    func tandemPump(_ pump: TandemPump, shouldConnect peripheral: CBPeripheral, advertisementData: [String: Any]?) -> Bool
+    func tandemPump(_ pump: TandemPump, didCompleteConfiguration peripheralManager: PeripheralManager)
+}
+
 
 public class TandemPump {
     private let log = OSLog(category: "TandemPump")
@@ -18,6 +23,9 @@ public class TandemPump {
 
     private var state: PumpState?
     private var appInstanceId = 1
+    private var currentTxId: UInt8 = 0
+
+    weak var delegate: TandemPumpDelegate?
 
     var pumpComm: PumpComm
 
@@ -26,6 +34,11 @@ public class TandemPump {
 
         self.pumpComm = PumpComm(pumpState: state)
         self.bluetoothManager.delegate = self
+    }
+
+    /// Start scanning for a Tandem pump peripheral.
+    public func startScanning() {
+        bluetoothManager.scanForPeripheral()
     }
 
     // MARK: - Configuration helpers
@@ -68,4 +81,50 @@ extension TandemPump: BluetoothManagerDelegate {
     func bluetoothManager(_ manager: BluetoothManager, didCompleteConfiguration peripheralManager: PeripheralManager) {
         // Intentionally left blank
     }
+    // MARK: - Bluetooth events
+
+    func onPumpConnected(_ manager: PeripheralManager) {
+        sendDefaultStartupRequests(manager)
+    }
+
+    private func sendDefaultStartupRequests(_ manager: PeripheralManager) {
+        let apiReq = ApiVersionRequest(cargo: Data())
+        let tsrReq = TimeSinceResetRequest(cargo: Data())
+        let requests: [Message] = [apiReq, tsrReq]
+        for req in requests {
+            send(req, via: manager)
+        }
+    }
+
+    public func sendCommand(_ message: Message, using manager: PeripheralManager) {
+        send(message, via: manager)
+    }
+
+    private func send(_ message: Message, via manager: PeripheralManager) {
+        let wrapper = TronMessageWrapper(message: message, currentTxId: currentTxId)
+        currentTxId &+= 1
+        manager.perform { pm in
+            _ = pm.sendMessagePackets(wrapper.packets)
+        }
+    }
+
 }
+
+extension TandemPump: BluetoothManagerDelegate {
+    func bluetoothManager(_ manager: BluetoothManager, peripheralManager: PeripheralManager, isReadyWithError error: Error?) {
+        guard error == nil else { return }
+        onPumpConnected(peripheralManager)
+    }
+
+    func bluetoothManager(_ manager: BluetoothManager, shouldConnectPeripheral peripheral: CBPeripheral, advertisementData: [String : Any]?) -> Bool {
+        if let delegate = delegate {
+            return delegate.tandemPump(self, shouldConnect: peripheral, advertisementData: advertisementData)
+        }
+        return true
+    }
+
+    func bluetoothManager(_ manager: BluetoothManager, didCompleteConfiguration peripheralManager: PeripheralManager) {
+        delegate?.tandemPump(self, didCompleteConfiguration: peripheralManager)
+    }
+}
+
