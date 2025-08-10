95a872d19d2fd946f0f3e316e64a77ec482568de
James Woglom <j@wogloms.net>
2025-08-10 15:27:36 -0400

    more messages

---
+++ TandemKit/Messages/ControlStream/NonexistentFillTubingStateStream.swift
@@ -0,0 +1,62 @@
+//
+//  NonexistentFillTubingStateStream.swift
+//  TandemKit
+//
+//  Created by OpenAI's Codex.
+//
+//  Swift representations of NonexistentFillTubingStateStreamRequest and FillTubingStateStreamResponse based on
+//  https://github.com/jwoglom/pumpX2/blob/main/messages/src/main/java/com/jwoglom/pumpx2/pump/messages/request/controlStream/NonexistentFillTubingStateStreamRequest.java
+//  https://github.com/jwoglom/pumpX2/blob/main/messages/src/main/java/com/jwoglom/pumpx2/pump/messages/response/controlStream/FillTubingStateStreamResponse.java
+//
+import Foundation
+
+/// Placeholder request for FillTubingStateStreamResponse which has no originating request.
+public class NonexistentFillTubingStateStreamRequest: Message {
+    public static var props = MessageProps(
+        opCode: 0,
+        size: 0,
+        type: .Request,
+        characteristic: .CONTROL_STREAM_CHARACTERISTICS,
+        stream: true,
+        signed: true
+    )
+
+    public var cargo: Data
+
+    public required init(cargo: Data) {
+        self.cargo = cargo
+    }
+
+    public init() {
+        self.cargo = Data()
+    }
+}
+
+/// Stream message received while tubing is being filled.
+public class FillTubingStateStreamResponse: Message {
+    public static var props = MessageProps(
+        opCode: UInt8(bitPattern: Int8(-27)),
+        size: 1,
+        type: .Response,
+        characteristic: .CONTROL_STREAM_CHARACTERISTICS,
+        stream: true,
+        signed: true
+    )
+
+    public var cargo: Data
+    public var buttonState: Int
+
+    public required init(cargo: Data) {
+        let raw = Bytes.dropLastN(cargo, 0)
+        self.cargo = raw
+        self.buttonState = Int(raw[0])
+    }
+
+    public init(buttonState: Int) {
+        self.cargo = Data([UInt8(buttonState & 0xFF)])
+        self.buttonState = buttonState
+    }
+
+    public var buttonDown: Bool { buttonState == 1 }
+}
+
