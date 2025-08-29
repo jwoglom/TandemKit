95fb0fbb8a843a15b64ca0ca2ba4dd840fe2f90d
James Woglom <j@wogloms.net>
2025-08-29 00:57:22 -0400

    Add JPAKE-based pairing session scaffolding

---
+++ Sources/TandemKit/PumpManager/PumpCommSession.swift
@@ -0,0 +1,45 @@
+#if canImport(HealthKit)
+import Foundation
+import TandemCore
+
+protocol PumpCommSessionDelegate: AnyObject {
+    func pumpCommSession(_ pumpCommSession: PumpCommSession, didChange state: PumpState)
+}
+
+public class PumpCommSession {
+    private let sessionQueue = DispatchQueue(label: "com.jwoglom.TandemKit.PumpCommSession.queue")
+    private(set) var state: PumpState
+    weak var delegate: PumpCommSessionDelegate?
+
+    public init(pumpState: PumpState, delegate: PumpCommSessionDelegate? = nil) {
+        self.state = pumpState
+        self.delegate = delegate
+    }
+
+    public func runSession(withName name: String, _ block: @escaping @Sendable () -> Void) {
+        sessionQueue.async(execute: block)
+    }
+
+    public func assertOnSessionQueue() {
+        dispatchPrecondition(condition: .onQueue(sessionQueue))
+    }
+
+    #if canImport(SwiftECC) && canImport(BigInt) && canImport(CryptoKit)
+    public func pair(transport: PumpMessageTransport, pairingCode: String) throws {
+        assertOnSessionQueue()
+        let builder = JpakeAuthBuilder.initializeWithPairingCode(pairingCode)
+        while !builder.done() && !builder.invalid() {
+            guard let request = builder.nextRequest() else { break }
+            let response = try transport.sendMessage(request)
+            builder.processResponse(response)
+        }
+        guard builder.done(), let secret = builder.getDerivedSecret() else {
+            throw PumpCommError.missingAuthenticationKey
+        }
+        state.derivedSecret = secret
+        state.serverNonce = builder.getServerNonce()
+        delegate?.pumpCommSession(self, didChange: state)
+    }
+    #endif
+}
+#endif
--- Sources/TandemKit/PumpManager/PumpState.swift
+++ Sources/TandemKit/PumpManager/PumpState.swift
@@ -5,36 +5,49 @@
 //  Created by James Woglom on 1/5/25.
 //
 
+import Foundation
+
 public struct PumpState: RawRepresentable, Equatable, CustomDebugStringConvertible {
     
     public typealias RawValue = [String: Any]
     
     public let address: UInt32
-    
-    public init(address: UInt32) {
+    public var derivedSecret: Data?
+    public var serverNonce: Data?
+
+    public init(address: UInt32, derivedSecret: Data? = nil, serverNonce: Data? = nil) {
         self.address = address
+        self.derivedSecret = derivedSecret
+        self.serverNonce = serverNonce
     }
     
     public init?(rawValue: RawValue) {
         
         guard let address = rawValue["address"] as? UInt32
         else { return nil }
-        
+
         self.address = address
+        self.derivedSecret = rawValue["derivedSecret"] as? Data
+        self.serverNonce = rawValue["serverNonce"] as? Data
     }
     
     public var rawValue: RawValue {
         var rawValue: RawValue = [
             "address": address,
         ]
-        
+
+        rawValue["derivedSecret"] = derivedSecret
+        rawValue["serverNonce"] = serverNonce
+
         return rawValue
     }
     
     public var debugDescription: String {
         return [
             "### PumpState",
-            "* address: \(String(format: "%04X", address))"
+            "* address: \(String(format: "%04X", address))",
+            "* hasDerivedSecret: \(derivedSecret != nil)",
+            "* hasServerNonce: \(serverNonce != nil)"
         ].joined(separator: "\n")
     }
 }
