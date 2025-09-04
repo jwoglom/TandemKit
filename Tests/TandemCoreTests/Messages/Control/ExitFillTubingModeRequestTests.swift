801f8f32a7a8c505d9a0d566f7d4a0c183471e97
James Woglom <j@wogloms.net>
2025-09-03 20:33:47 -0400

    Port PumpX2 control request tests

---
+++ Tests/TandemCoreTests/Messages/Control/ExitFillTubingModeRequestTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import TandemCore
+
+final class ExitFillTubingModeRequestTests: XCTestCase {
+    func testExitFillTubingModeRequest() {
+        MessageTester.initPumpState("", 0)
+        let expected = ExitFillTubingModeRequest()
+
+        let parsed: ExitFillTubingModeRequest = MessageTester.test(
+            "01919691181b0f4120ad6869443532522e06f02a",
+            -111,
+            1,
+            .CONTROL_CHARACTERISTICS,
+            expected,
+            "0091687757e745d1f795a3295f"
+        )
+
+        MessageTester.assertHexEquals(expected.cargo, parsed.cargo)
+    }
+}
