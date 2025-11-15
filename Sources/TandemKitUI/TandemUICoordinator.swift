cf0232a9a36705869766500a00b6d022dce52454
bastiaanv <bastiankpn7800@gmail.com>
2025-11-15 22:26:08 +0100

    add UI example

--- Sources/TandemKitUI/TandemUICoordinator.swift
+++ Sources/TandemKitUI/TandemUICoordinator.swift
@@ -1,10 +1,20 @@
 import LoopKit
+import SwiftUI
 import LoopKitUI
+
+enum TandemScreen {
+    case onboarding
+}
 
 class TandemUICoordinator: UINavigationController, PumpManagerOnboarding, CompletionNotifying, UINavigationControllerDelegate {
     var pumpManagerOnboardingDelegate: PumpManagerOnboardingDelegate?
 
     var completionDelegate: CompletionDelegate?
+    
+    var screenStack = [TandemScreen]()
+    var currentScreen: TandemScreen {
+        screenStack.last!
+    }
 
     private let colorPalette: LoopUIColorPalette
 
@@ -41,5 +51,29 @@ class TandemUICoordinator: UINavigationController, PumpManagerOnboarding, Comple
 
     @available(*, unavailable) required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if screenStack.isEmpty {
+            screenStack = [.onboarding]
+            let viewController = viewControllerForScreen(currentScreen)
+            viewController.isModalInPresentation = false
+            setViewControllers([viewController], animated: false)
+        }
+    }
+
+    private func hostingController<Content: View>(rootView: Content) -> DismissibleHostingController<some View> {
+        let rootView = rootView
+            .environment(\.appName, Bundle.main.bundleDisplayName)
+        return DismissibleHostingController(content: rootView, colorPalette: colorPalette)
+    }
+    
+    private func viewControllerForScreen(_ screen: TandemScreen) -> UIViewController {
+        switch screen {
+        case .onboarding:
+            return hostingController(rootView: TandemOnboardingView())
+        }
     }
 }
---
+++ Sources/TandemKitUI/Views/TandemOnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  TandemOnboardingView.swift
+//  TandemKit
+//
+//  Created by Bastiaan Verhaar on 15/11/2025.
+//
+
+import SwiftUI
+
+struct TandemOnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TandemOnboardingView()
+}
