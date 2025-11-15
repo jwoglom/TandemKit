155649e71001cbb118cdfa73e0de2d490dcb96d1
James Woglom <j@wogloms.net>
2025-11-15 13:22:42 -0500

    add Info.plist and header file

---
+++ Sources/TandemKitPlugin/TandemKitPlugin.h
@@ -0,0 +1,19 @@
+//
+//  DanaKitPlugin.h
+//  DanaKitPlugin
+//
+//  Created by James Woglom on 11/15/25.
+//  Copyright © 2025 LoopKit Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DanaKitPlugin.
+FOUNDATION_EXPORT double TandemKitPluginVersionNumber;
+
+//! Project version string for DanaKitPlugin.
+FOUNDATION_EXPORT const unsigned char TandemKitPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TandemKitPlugin/PublicHeader.h>
+
+
